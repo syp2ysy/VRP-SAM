643d7850deafea5f28d92203dee76f6b838f24e4
xiaoxiao <39050296+syp2ysy@users.noreply.github.com>
2024-07-15 20:54:54 +0800

    Update logger.py

--- common/logger.py
+++ common/logger.py
@@ -111,10 +111,10 @@ class Logger:
         cls.tbd_writer = SummaryWriter(os.path.join(cls.logpath, 'tbd/runs'))
 
         # Log arguments
-        logging.info('\n:=========== Few-shot Seg. with HSNet ===========')
+        logging.info('\n:=========== Few-shot Seg. with VRP-SAM ===========')
         for arg_key in args.__dict__:
             logging.info('| %20s: %-24s' % (arg_key, str(args.__dict__[arg_key])))
-        logging.info(':================================================\n')
+        logging.info(':==================================================\n')
 
     @classmethod
     def info(cls, msg):
