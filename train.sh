9c53a6a43373165d7cd185d96707b6a4d6665413
xiaoxiao <39050296+syp2ysy@users.noreply.github.com>
2024-09-27 21:43:21 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -2,11 +2,9 @@
 # sleep 6h
 PARTITION=Segmentation
 
-# GPU_ID=0,1,2,3
-GPU_ID=0
+GPU_ID=0,1,2,3
 
-
-CUDA_VISIBLE_DEVICES=${GPU_ID} python3 -m torch.distributed.launch --nproc_per_node=1 --master_port=6224 train.py \
+CUDA_VISIBLE_DEVICES=${GPU_ID} python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=6224 train.py \
                         --epochs 50 \
                         --condition mask \
                         --lr 1e-4 \
